35de418e3ffc1a6fac5b273af1c1ffe514e39a2c
JT9 <caleb.tsosie@asu.edu>
2017-10-09 01:22:25 -0700

    Setup more of the ScrollView
    Add the scrollview to show images in the array when you swipe left

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -10,20 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     //Empty array
     
     var images = [UIImageView]()
     
     
+    //print("ScrollView width: \(scrollView.frame.size.width)")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
+        
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2) - 75, width: 150, height: 150)
+            
+            
+            
         }
         
-        print("Count: \(images.count)")
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
 
